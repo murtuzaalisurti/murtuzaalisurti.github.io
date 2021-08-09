6711381e075e296344e781f2dd0d68cb68046b1a
murtuza <murtuza.creativity@gmail.com>
2021-08-09 17:20:08 +0530

    fixed js bug

--- js/browser.js
+++ js/browser.js
@@ -16,8 +16,39 @@ var isEdge = !isIE && !!window.StyleMedia;
 // Chrome 1 - 71
 var isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
 
+var isChromium = window.chrome;
+var winNav = window.navigator;
+var vendorName = winNav.vendor;
+var IsOpera = typeof window.opr !== "undefined";
+var isIEedge = winNav.userAgent.indexOf("Edge") > -1;
+var isIOSChrome = winNav.userAgent.match("CriOS");
+
+if (isIOSChrome) {
+   // is Google Chrome on IOS
+} else if(
+  isChromium !== null &&
+  typeof isChromium !== "undefined" &&
+  vendorName === "Google Inc." &&
+  IsOpera === false &&
+  isIEedge === false
+) {
+   // is Google Chrome
+} else { 
+   // not Google Chrome 
+}
+
 // Blink engine detection
 var isBlink = (isChrome || isOpera) && !!window.CSS;
+
+if(isSafari || isFirefox){
+    let unobfus_script = document.createElement("script");
+    unobfus_script.src = "./js/main1.js";
+    document.body.appendChild(unobfus_script);
+} else{
+    let obfus_script = document.createElement("script");
+    obfus_script.src = "./js/main.js";
+    document.body.appendChild(obfus_script);
+}
 // if(isFirefox || isIE){
 //     // document.querySelector("main").style = `display: none;`;
 //     // document.querySelector(".header-contain").style = `display: none;`;
