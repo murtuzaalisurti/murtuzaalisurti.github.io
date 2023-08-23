1eec89265cc47c5f53c6093a8b59525ee2477e2c
Murtuzaali Surti <68743212+murtuzaalisurti@users.noreply.github.com>
2023-08-23 23:34:41 +0530

    new site

--- LICENSE.md
+++
@@ -1,5 +0,0 @@
-# License
-
-This project is neither for commercial use nor is it for personal use. Distribution, patent, misuse is strictly prohibited.
-
-&copy;2021 All rights reserved.
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-    site: "https://murtuzaalisurti.github.io"
-});
+export default defineConfig({});
