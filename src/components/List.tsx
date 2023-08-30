4416a3fedb6cbbaa6607f368ce922ac4a2fd2e6a
Murtuzaali Surti <68743212+murtuzaalisurti@users.noreply.github.com>
2023-08-30 23:06:32 +0530

    progress

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,12 +1,11 @@
-import React from "react";
 import type { TListPropsWithChildren } from "../lib/types";
 
 const List = ({ children, list, section }: TListPropsWithChildren) => {
     if (section === "experience" || section === "projects") {
         return (
             <>
-                <h3>{section.charAt(0).toUpperCase() + section.slice(1)}</h3>
-                <div id="container">
+                <h3>{section.charAt(0).toUpperCase() + section.slice(1)}<hr /></h3>
+                <div className="listContainer">
                     {
                         list.map(entry => {
                             return (
