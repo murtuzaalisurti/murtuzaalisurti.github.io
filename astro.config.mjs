f960700352ecf897855904c97b317cccc0dfe4d7
Murtuzaali Surti <68743212+murtuzaalisurti@users.noreply.github.com>
2023-09-24 13:11:37 +0530

    sitemap fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://murtuzaalisurti.github.io",
   integrations: [
     react(),
     sitemap({
