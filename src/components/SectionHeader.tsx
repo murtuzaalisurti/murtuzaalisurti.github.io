150142d3a907bfe6ff4983928b16152dfd42f437
Murtuzaali Surti <68743212+murtuzaalisurti@users.noreply.github.com>
2023-10-02 13:04:30 +0530

    added link

--- src/components/SectionHeader.tsx
+++ src/components/SectionHeader.tsx
@@ -1,9 +1,12 @@
 import type { TSectionHeaderWithChildren } from "../lib/types"
 
-const SectionHeader = ({children, heading, emoji}: TSectionHeaderWithChildren) => {
+const SectionHeader = ({ children, heading, emoji }: TSectionHeaderWithChildren) => {
     return (
         <>
             <h3 className="section-heading">
+                <span className="headingAnchorLink">
+                    <a href={`#${heading}`}><sup>🔗</sup></a>
+                </span>
                 {heading.charAt(0).toUpperCase() + heading.slice(1)}
                 <hr />
                 <span>{emoji}</span>
