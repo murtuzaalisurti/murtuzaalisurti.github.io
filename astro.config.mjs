d33e9e472765c2d3140f9621fde7785dc67887df
Murtuzaali Surti <68743212+murtuzaalisurti@users.noreply.github.com>
2024-04-21 16:31:52 +0530

    refactor: remove react (#20)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,16 @@
 import { defineConfig } from 'astro/config';
-import million from 'million/compiler';
-import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://murtuzaalisurti.github.io",
   integrations: [
-    react(),
     sitemap({
       changefreq: "monthly",
       lastmod: new Date(),
       priority: 1.0
     })
   ],
-  vite: {
-    plugins: [
-      million.vite({
-        mode: 'react',
-        server: true
-      })
-    ]
-  },
   server: {
     port: 3000
   }
--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-    return (
-        <footer>
-            <p>&copy;<span id="currentYear"></span>. Made with 💖 by <a href="https://github.com/murtuzaalisurti">Murtuzaali</a>.</p>
-        </footer>
-    )
-}
-
-export default Footer
