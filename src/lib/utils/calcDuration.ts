c2f10ac5ae6e8abcfb9183705ee548cce5179e22
Murtuzaali Surti <68743212+murtuzaalisurti@users.noreply.github.com>
2024-09-13 23:47:45 +0530

    feat: migrate to dayjs from luxon and improve calc duration

--- src/lib/utils/calcDuration.ts
+++ src/lib/utils/calcDuration.ts
@@ -1,5 +1,27 @@
-import { DateTime } from "luxon";
+import dayjs from "dayjs";
 import { getDurationString } from "./common";
+
+/**
+ * Takes a date string in the format "MM/YY" and converts it to an array
+ * of strings in the format ["MM", "YYYY"]. If `present` is true, the year
+ * is not prepended with "20".
+ *
+ * @param {string} dateString - The date string to convert.
+ * @param {boolean} [present=false] - Whether the year should not be prepended with
+ * "20". Defaults to false.
+ * @return {string[]} An array of strings in the format ["MM", "YYYY"].
+ */
+const getMonthAndYearArray = (dateString: string, present: boolean = false) => {
+    return dateString.split("/")
+        .filter((i) => i !== " ")
+        .map((e) => e.trim().padStart(2, "0"))
+        .map((k, i, arr) => {
+            if (!present) {
+                if (i === arr.length - 1) return `20${k}`;
+            }
+            return k;
+        });
+}
 
 /**
  * Calculates the duration between two dates and returns an object with the
@@ -11,38 +33,24 @@ import { getDurationString } from "./common";
  * formatted HTML string.
  */
 export const calcDuration = (startDate: string, endDate: string) => {
-    const ArrayStartDate = startDate
-        .split(",")
-        .filter((i) => i !== " ")
-        .map((e) => e.trim());
+    const ArrayStartDate = getMonthAndYearArray(startDate);
 
     const ArrayEndDate =
         endDate === "Present"
-            ? `${new Date().toLocaleString("default", {
-                month: "short",
-            })}, ${new Date().getFullYear()}`
-                .split(",")
-                .filter((i) => i !== " ")
-                .map((e) => e.trim())
-            : endDate
-                .split(",")
-                .filter((i) => i !== " ")
-                .map((e) => e.trim());
+            ? getMonthAndYearArray(`${new Date().getMonth() + 1}/${new Date().getFullYear()}`, true)
+            : getMonthAndYearArray(endDate);
 
-    ArrayStartDate.unshift("1");
-    ArrayEndDate.unshift("1");
+    // new Date() accepts YYYY-MM format
+    const EndDate = (dayjs(new Date(ArrayEndDate.reverse().join("-"))));
+    const StartDate = (dayjs(new Date(ArrayStartDate.reverse().join("-"))));
 
-    const EndDate = (DateTime.fromJSDate(new Date(ArrayEndDate.join(" "))));
-    const StartDate = (DateTime.fromJSDate(new Date(ArrayStartDate.join(" "))));
-
-    const diffObject = EndDate.diff(StartDate, ["years", "months"]);
-    const diffYears = diffObject.years;
-    const diffMonths = diffObject.months;
+    const diffYears = EndDate.diff(StartDate, 'years');
+    const diffMonths = EndDate.diff(StartDate, 'months') - (diffYears * 12);
 
     return {
         diffYears,
         diffMonths,
-        html: `${startDate} - ${endDate} • 
+        html: `${StartDate.format("MMM, YYYY")} - ${endDate === "Present" ? "Present" : EndDate.format("MMM, YYYY")} • 
         ${getDurationString(diffYears, diffMonths)}`
     };
 };
