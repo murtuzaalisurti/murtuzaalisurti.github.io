a2dad561e716be8f9df8f442746b1b24a3424bf3
murtuzaalisurti <murtuza.creativity@gmail.com>
2025-10-24 23:57:01 +0530

    fix: update domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://murtuzaalisurti.github.io",
+  site: "https://murtuzaalisurti.com",
   integrations: [
     sitemap({
       changefreq: "monthly",
